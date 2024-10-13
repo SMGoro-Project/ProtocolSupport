1dbca7733139662870a59cf4a4a3d16f55625fcf
RENaa_FD <92320175+lRENyaaa@users.noreply.github.com>
2024-10-13 16:32:16 +0800

    Fix support with viaversion

--- src/main/java/protocolsupport/ProtocolSupport.java
+++ src/main/java/protocolsupport/ProtocolSupport.java
@@ -1,8 +1,13 @@
 package protocolsupport;
 
 import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import protocolsupport.api.events.PlayerLoginStartEvent;
 import protocolsupport.commands.CommandHandler;
 import protocolsupport.injector.ServerInjector;
 import protocolsupport.injector.network.NettyInjector;
@@ -17,26 +22,32 @@ import protocolsupport.utils.netty.Compressor;
 public class ProtocolSupport extends JavaPlugin {
 
 	@Override
-	public void onLoad() {
-		try {
-			Allocator.init();
-			Compressor.init();
-			ServerBoundPacket.init();
-			ClientBoundPacket.init();
-			InitialPacketDecoder.init();
-			AbstractLoginListener.init();
-			NettyInjector.inject();
-			ServerInjector.inject();
-		} catch (Throwable t) {
-			t.printStackTrace();
-			Bukkit.shutdown();
-		}
-	}
-
-	@Override
 	public void onEnable() {
-		getCommand("protocolsupport").setExecutor(new CommandHandler());
-		getServer().getPluginManager().registerEvents(new PlayerListener(), this);
+		Listener listener = new Listener() {
+			@EventHandler
+			public void onLogin(PlayerLoginEvent event) {
+				event.disallow(PlayerLoginEvent.Result.KICK_OTHER, "Server is still loading!");
+			}
+		};
+		Bukkit.getPluginManager().registerEvents(listener, this);
+		Bukkit.getScheduler().runTask(this, () -> {
+			try {
+				Allocator.init();
+				Compressor.init();
+				ServerBoundPacket.init();
+				ClientBoundPacket.init();
+				InitialPacketDecoder.init();
+				AbstractLoginListener.init();
+				NettyInjector.inject();
+				ServerInjector.inject();
+			} catch (Throwable t) {
+				t.printStackTrace();
+				Bukkit.shutdown();
+			}
+			getCommand("protocolsupport").setExecutor(new CommandHandler());
+			getServer().getPluginManager().registerEvents(new PlayerListener(), this);
+			HandlerList.unregisterAll(listener);
+		});
 	}
 
 	@Override
